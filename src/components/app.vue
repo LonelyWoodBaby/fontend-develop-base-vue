103394d2740f7888dcfc7a83b878af369fdb3c53
LeeYarbin <lyb429958@126.com>
2018-02-01 02:13:51 +0800

    完成一级菜单的切换工作，并成功从服务器端获取到数据

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="daily">
     <div class="daily-menu">
-      <div class="daily-menu-item">每日推荐</div>
-      <div class="daily-menu-item">主题日报</div>
+      <div class="daily-menu-item" 
+        :class="{on:type==='recommend'}"
+        @click="handleToRecommend"
+      >每日推荐</div>
+      <div class="daily-menu-item" 
+        :class="{on:type==='daily'}"
+        @click="showThemes = !showThemes">主题日报</div>
+      <ul v-show="showThemes">
+        <li v-for="item in themes" :key="item.id">
+          <a :class="{on:item.id === themeId && type === 'daily'}" @click="handleToTheme(item.id)">{{item.name}}</a>
+        </li>
+      </ul>
     </div>
     <div class="daily-list">
       <!--
@@ -15,12 +25,58 @@
   </div>
 </template>
 <script>
+import $ from '../libs/util.js';
 export default {
   data() {
-    return {};
+    return {
+      themes:[],
+      showThemes:false,
+      type:'recommend',
+      themeId:0,
+
+      list:[],
+
+      recommendList:[],
+      dailyTime:$.getTodayTime(),
+      isLoading:false
+
+    };
   },
-  mounted() {},
+  mounted() {
+    this.getThemes();
+    this.getRecommendList();
+  },
   beforeDestroy() {},
-  methods: {}
+  methods: {
+    getThemes (){
+      $.ajax.get('themes').then(res =>{
+        this.themes = res.others;
+      });
+    },
+    handleToTheme (id){
+      this.type = 'daily';
+      this.themeId = id;
+      this.list = [];
+      $.ajax.get('theme/' +id).then(res =>{
+        this.list = res.stories.filter(item => item.type !== 1);
+      });
+    },
+    handleToRecommend (){
+      this.type = 'recommend';
+      this.recommendList =[];
+      this.dailyTime = $.getTodayTime();
+      this.getRecommendList();
+    },
+    getRecommendList (){
+      this.isLoading = true;
+      const preDay = $.getPreDay(this.dailyTime + 86400000);
+     
+      $.ajax.get('news/before/' + preDay).then(res =>{
+        console.log(res);
+        this.recommendList.push(res);
+        this.isLoading = false;
+      });
+    }
+  }
 };
 </script>
--- src/libs/util.js
+++ src/libs/util.js
@@ -11,5 +11,25 @@ Util.ajax = axios.create({
 Util.ajax.interceptors.response.use(res=>{
   return res.data;
 });
+Util.getTodayTime = function(){
+  const date = new Date();
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date.getTime();
+};
+
+Util.getPreDay = function(timestamp = (new Date()).getTime){
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1 < 10 
+    ? '0' + (date.getMonth() + 1)
+    : date.getMonth() + 1;
+  const day = date.getDate() < 10 
+    ? '0' + (date.getDate())
+    : date.getDate() + 1;
+  return year + month + day;
+};
 
 export default Util;
