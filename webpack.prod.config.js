fbca8fecdd9ff21d6a01d45de11cc39228793138
LeeYarbin <lyb429958@126.com>
2018-01-31 14:04:21 +0800

    vue前端基础配置依赖

---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+let ExtractTextPlugun = require("extract-text-webpack-plugin");
+let merge = require("webpack-merge");
+let WebpackBaseConfig = require("./webpack.config.js");
+
+//清空基本配置的插件
+WebpackBaseConfig.plugins = [];
+module.exports = merge(WebpackBaseConfig,{
+    output:{
+        publicPath:"/dist/",
+        filename : '[name].[hash].js'
+    },
+    plugins:[
+        new ExtractTextPlugun({
+            filename:'[name].[hash].css',
+            allChunks:true
+        }),
+        new webpack.DefinePlugin({
+            'progress.env':{
+                NODE_ENV:'"production"'
+            }
+        }),
+        //压缩js
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings:false
+            }
+        }),
+        new htmlWebpackPlugin({
+            filename:"./src/index_prod.html",
+            template:"./src/templates/index.ejs",
+            inject:false
+        })
+    ]
+});
